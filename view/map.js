f9c28f6e3227a4a51ae04409397f8910c81bebc8
Vladimir Kochubenko <v.kochubenko@mobidev.biz>
2015-03-24 19:21:30 +0200

    first commit

--- view/map.js
+++ view/map.js
@@ -1,4 +1,14 @@
 (function ($){
+    var Model = Backbone.Model.extend({
+        defaults:{
+            item1:'<img style="width: 100px; height: 100px" src="link.gif">',
+            item2:'gnom'
+        }
+    });
+
+    var ModelList = Backbone.Collection.extend({
+        model: Model
+    });
     var MapList = Backbone.View.extend({
         el: $('body'),
         events: {
@@ -7,18 +17,35 @@
 
         initialize: function () {
             _.bindAll(this, 'render');
+            this.collection = new ModelList();
+            this.collection.bind('add', this.appendItem);
+            this.counter = 0;
             this.render();
         },
 
         render: function () {
+            var self = this;
             $(this.el).append('<button id="add">Add</button>');
-            $(this.el).append('<ul> <li></li> </ul>');
+            $(this.el).append('<ul></ul>');
+            _(this.collection.models).each(function(item){
+                self.appendItem(item);
+            }, this);
         },
 
         addItem: function () {
             this.counter++;
-            $('ul', this.el).append('<li><img style="width: 100px; height: 100px" src="link.gif"><img style="width: 100px; height: 100px" src="link.gif"><img style="width: 100px; height: 100px" src="link.gif"><img style="width: 100px; height: 100px" src="link.gif"><img style="width: 100px; height: 100px" src="link.gif"><img style="width: 100px; height: 100px" src="link.gif"></li>');
+            var model = new Model();
+            //$('ul', this.el).append('<li></li>');
+            model.set({
+                item2: model.get('item2') + this.counter
+            })
+            this.collection.add(model);
+        },
+
+        appendItem: function (item) {
+            $('ul', this.el).append('<li>'+item.get('item2')+' '+item.get('item1')+'</li>');
         }
+
     });
 
     var mapList = new MapList();
